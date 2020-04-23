22ec9f5ab380623ed9fadb80c856bc0500d3c712
boming zhang <z814491105@gmail.com>
2020-04-23 13:58:23 -0400

    Add centimeter reader and play next gesture

--- llap/ViewController.m
+++ llap/ViewController.m
@@ -9,12 +9,17 @@
 #import "ViewController.h"
 #import "AudioController.h"
 
+
 @interface ViewController (){
     AudioController *audioController;
 }
 
+@property (weak, nonatomic) IBOutlet UILabel *gesture;
+
+@property (weak, nonatomic) IBOutlet UILabel *distanceRead;
 @property (weak, nonatomic) IBOutlet UISlider *slider;
 @end
+int lastTime = 0;
 
 @implementation ViewController
 
@@ -24,6 +29,8 @@
     audioController = [[AudioController alloc] init];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(performDisUpdate:) name:@"AudioDisUpdate" object:nil];
     [_slider setValue: 0.0];
+    [_distanceRead setText: @"0"];
+    _gesture.text = @"Ready for recognizing the gesture";
     [self.view addSubview: _slider];
 }
 - (IBAction)playbutton:(UIButton *)sender {
@@ -32,6 +39,7 @@
     
 }
 - (IBAction)stopbutton:(UIButton *)sender {
+    _gesture.text = @"Ready for the next gesture";
     [audioController stopIOUnit];
 }
 
@@ -45,8 +53,13 @@
     dispatch_async(dispatch_get_main_queue(), ^{
     
     int tempdis=(int) audioController.audiodistance/DISPLAY_SCALE;
-        
+        if ((audioController.audiodistance-lastTime)>5) {
+            _gesture.text = @"Play next";
+        }
      _slider.value=(audioController.audiodistance-DISPLAY_SCALE*tempdis)/DISPLAY_SCALE;
+        NSString* myNewString = [NSString stringWithFormat:@"%.01f", audioController.audiodistance];
+        _distanceRead.text=myNewString;
+        lastTime = audioController.audiodistance;
     }
         );
 
