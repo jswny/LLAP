0cd7ffca69c4e33f2cfc1ac08c09bee27df03957
Joe Sweeney <joe@j1.io>
2020-04-25 00:44:24 -0400

    Add Yanjie's code

--- llap/ViewController.m
+++ llap/ViewController.m
@@ -12,6 +12,7 @@
 
 @interface ViewController (){
     AudioController *audioController;
+    AVAudioPlayer *musicPlayer;
 }
 
 @property (weak, nonatomic) IBOutlet UILabel *gesture;
@@ -26,20 +27,26 @@ int lastTime = 0;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *file = [NSString stringWithFormat:@"%@/summer.mp3", [[NSBundle mainBundle] resourcePath]];
+    NSURL *music = [NSURL fileURLWithPath:file];
+    musicPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL: music error: nil];
     audioController = [[AudioController alloc] init];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(performDisUpdate:) name:@"AudioDisUpdate" object:nil];
     [_slider setValue: 0.0];
     [_distanceRead setText: @"0"];
-    _gesture.text = @"Ready for recognizing the gesture";
+    _gesture.text = @"Ready for Playing Music";
     [self.view addSubview: _slider];
 }
 - (IBAction)playbutton:(UIButton *)sender {
+    [musicPlayer play];
+    _gesture.text = @"Ready for Controlling Music Playing";
     audioController.audiodistance=0;
     [audioController startIOUnit];
     
 }
 - (IBAction)stopbutton:(UIButton *)sender {
-    _gesture.text = @"Ready for the next gesture";
+    [musicPlayer stop];
+    _gesture.text = @"Ready for Playing Music";
     [audioController stopIOUnit];
 }
 
@@ -53,7 +60,8 @@ int lastTime = 0;
     dispatch_async(dispatch_get_main_queue(), ^{
         int tempdis=(int) audioController.audiodistance/DISPLAY_SCALE;
         if ((audioController.audiodistance-lastTime)>5) {
-            _gesture.text = @"Play next";
+            _gesture.text = @"Music Paused";
+            [musicPlayer pause];
         }
         _slider.value=(audioController.audiodistance-DISPLAY_SCALE*tempdis)/DISPLAY_SCALE;
         NSString* myNewString = [NSString stringWithFormat:@"%.01f", audioController.audiodistance];
